0c6c8ca5b07ae225b4b4e30ec9cd121910fe0448
Park.Pat <90613277+ByeongJo-Park@users.noreply.github.com>
2023-09-13 16:47:01 +0900

    [Gold V] Title: 토마토, Time: 1112 ms, Memory: 219596 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -8,10 +8,11 @@ def bfs():
     global mn
     while que:
         i, j, k = que.popleft()
+        # 상하좌우위아래 체크 범위내라면 que에 입력,
         for di, dj, dk in [[0,0,1], [0, 0, -1], [0,1,0], [0, -1, 0], [-1,0,0], [1,0,0]]:
             ni, nj, nk = i + di, j + dj, k + dk
-            if 0 <= ni < h and 0 <= nj < n and 0 <= nk < m and arr[ni][nj][nk] == 0:
-                arr[ni][nj][nk] = 1
+            if 0 <= ni < h and 0 <= nj < n and 0 <= nk < m and arr[ni][nj][nk] == 0 and visited[ni][nj][nk] == 0:
+                # arr[ni][nj][nk] = 1
                 que.append((ni,nj,nk))
                 visited[ni][nj][nk] = visited[i][j][k] + 1
 
@@ -22,7 +23,7 @@ arr = [[list(map(int, input().strip().split())) for _ in range(n)] for k in rang
 
 visited = [[[0]*m for _ in range(n)] for _ in range(h)]
 
-# 익은 토마토들 동시에 돌려야할 듯...
+# 익은 토마토들 동시에 돌리기 위해 미리 que에 입력, 방문 체크열에도 체크
 que = deque()
 for i in range(h):
     for j in range(n):
@@ -33,7 +34,7 @@ for i in range(h):
 
 bfs()
 
-ans =  0
+ans = 0
 # 0있는지 확인 있으면 -1 출력, 있으면 최고값-1 출력
 for i in range(h):
     for j in range(n):
