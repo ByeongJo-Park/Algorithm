cb7ac0eb15860c3abe04e676eb079b87d4d8da5c
Park.Pat <90613277+ByeongJo-Park@users.noreply.github.com>
2024-02-21 10:55:13 +0800

    [Bronze II] Title: 재귀의 귀재, Time: 552 ms, Memory: 31180 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
@@ -0,0 +1,15 @@
+def recursion(s, l, r, c):
+    if l >= r:
+        return 1, c
+    elif s[l] != s[r]:
+        return 0, c
+    else:
+        return recursion(s, l+1, r-1, c+1)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1, 1)
+
+N = int(input())
+
+for _ in range(N):
+    print(*isPalindrome(input().strip()))
