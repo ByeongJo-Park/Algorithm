9538b1222fe1191b90aabed8f09bac7f5265b20e
Park.Pat <90613277+ByeongJo-Park@users.noreply.github.com>
2024-02-26 19:13:50 +0800

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 2168 ms, Memory: 142872 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.py
@@ -0,0 +1,44 @@
+import sys
+input = sys.stdin.readline
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = (len(arr) + 1) // 2
+    low_arr = merge_sort(arr[:mid])
+    high_arr = merge_sort(arr[mid:])
+
+    sorted_arr = []
+    l = h = 0
+    while l < len(low_arr) and h < len(high_arr):
+        if low_arr[l] <= high_arr[h]:
+            sorted_arr.append(low_arr[l])
+            ans.append(low_arr[l])
+            l += 1
+        else:
+            sorted_arr.append(high_arr[h])
+            ans.append(high_arr[h])
+            h += 1
+
+    while l < len(low_arr):
+        sorted_arr.append(low_arr[l])
+        ans.append(low_arr[l])
+        l += 1
+
+    while h < len(high_arr):
+        sorted_arr.append(high_arr[h])
+        ans.append(high_arr[h])
+        h += 1
+
+    return sorted_arr
+
+N, M = map(int, input().split())
+
+A = list(map(int, input().split()))
+
+count = 0
+
+ans = []
+merge_sort(A)
+
+print(ans[M-1] if M <= len(ans) else -1)
