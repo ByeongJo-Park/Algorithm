38703a008090b25fe7e10792cc4fc7ef5d2c3038
Park.Pat <90613277+ByeongJo-Park@users.noreply.github.com>
2024-02-27 19:52:41 +0800

    [Silver III] Title: N과 M (2), Time: 88 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,25 @@
+def dfs():
+    global ans
+    if len(s) == m:
+        k = sorted(list(s))
+        if k not in ans:
+            ans.append(sorted(list(s)))
+        return
+    for i in range(1, n + 1):
+        if visited[i]:
+            continue
+        visited[i] = True
+        s.append(i)
+        dfs()
+        s.pop()
+        visited[i] = False
+
+
+n, m = map(int, input().split())
+s = []
+ans = []
+visited = [False] * (n + 1)
+
+dfs()
+for i in range(len(ans)):
+    print(*ans[i])
