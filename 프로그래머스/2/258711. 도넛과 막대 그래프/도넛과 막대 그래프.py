0b45108924fbbfbdf9143b3482c034d7f7a247ab
Park.Pat <90613277+ByeongJo-Park@users.noreply.github.com>
2024-03-21 13:46:29 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -1,19 +1,13 @@
 def solution(edges):
-    answer = findCreate(edges)
-    return answer
-
-
-edge_1 = [[2, 3], [4, 3], [1, 1], [2, 1]]
-edge_2 = [[4, 11], [1, 12], [8, 3], [12, 7], [4, 2], [7, 11], [4, 8], [9, 6], [10, 11], [6, 10], [3, 5], [11, 1], [5, 3], [11, 9], [3, 8]]
-
-def findCreate(edges):
     l = len(edges)
 
     s_check = dict()
     e_check = dict()
+    
     create_node = 0
     stick_node = 0
     eight_node = 0
+    
     for i in range(l):
         s, e = edges[i]
         if s in s_check:
@@ -38,9 +32,3 @@ def findCreate(edges):
             stick_node += 1
 
     return [create_node, len(s_check[create_node]) - eight_node - stick_node, stick_node, eight_node]
-# 도넛(n) : n / n
-# 막대(n) : n / n - 1
-# 8자(n) : 2n + 1 / 2n + 2
-
-print(solution(edge_1))
-print(solution(edge_2))
